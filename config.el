9b56ff617fb29a464b77dafba25070c043488953
thedoa1013 <35462209+Thedoa1013@users.noreply.github.com>
2023-12-09 21:17:40 +0100

    First commit

---
+++ config.el
@@ -0,0 +1,117 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;;; This makes all emacs frames that open to be maximized.
+;;;(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;;; This would make only the first frame that opens to be maximized
+;;; But when you open it in client mode, it doesnt work as the first frame created is the daemon
+;;; and subsequent frames are clients and don't get maximized.
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-material)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+
+;;; LSP OPTIONS
+
+;;; clangd is a Language Server Protocol for C++. This sets its defaults.
+(after! lsp-clangd
+  (setq lsp-clients-clangd-args
+        '("-j=3"
+          "--background-index"
+          "--clang-tidy"
+          "--completion-style=detailed"
+          "--header-insertion=never"
+          "--header-insertion-decorators=0"))
+  (set-lsp-priority! 'clangd 2))
+
+;;; Digestif is a latex server protocol. There seems to be another one that's more popular, but it looked harder to install so I went with this one.
+(setq lsp-tex-server 'digestif)
+
+;;; Some Language Server Protocols include formatting options.
+;;; Sometime they only allow a certain version of the formater (ccls, clang-format).
+;;; This line deactivates formatting by the lsp globally so that the format module takes care of it.
+(setq +format-with-lsp nil)
+
+
+;;; This will set specific major mode for spedific files.
+;;; This allows -among other stuff- for lsp to work.
+(add-to-list 'auto-mode-alist '("\\.clang-format\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.cmake-format\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.pro\\'" . qt-pro-mode))
+(add-to-list 'auto-mode-alist '("\\.tpp\\'" . cpp-mode))
+
+;;; This sets the folder where projectile (the project manager) will look for files.
+(setq projectile-project-search-path '("~/code/"))
+
+
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
